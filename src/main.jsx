3e040bd4d373f07c68b1527af87961a423c2bb9a
mansms <47422885+mansMS@users.noreply.github.com>
2025-11-08 17:45:46 +0300

    Add manifest files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+
+import './index.css'
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/taxi-front/sw.js')
+      .then((reg) => console.log('SW registered', reg))
+      .catch((err) => console.log('SW registration failed', err));
+  });
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
