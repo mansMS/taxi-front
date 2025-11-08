1d13e8b7a4b8d633ec34fd7a1aaf9e3123b984ae
mansms <47422885+mansMS@users.noreply.github.com>
2025-11-08 23:07:33 +0300

    chore: text forms

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,72 @@ import viteLogo from '/icon-192x192.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fromRoute, setFromRoute] = useState('')
+  const [selectedDriverIds, setSelectedDriverIds] = useState([])
+
+  const drivers = [
+    { id: 1, name: 'Паша', car: 'Merkedek', carNumber: '980', carColor: 'Белый' },
+    { id: 2, name: 'Мого', car: 'Самолет', carNumber: 'О634ЕА', carColor: 'Черный' },
+    { id: 3, name: 'Марат', car: 'Самокат', carNumber: 'У980ОТ', carColor: 'Серый' },
+    { id: 4, name: 'Челобай', car: 'Гьоко', carNumber: '123', carColor: 'Белый' },
+    { id: 5, name: 'Паша', car: 'Merkedek', carNumber: '980', carColor: 'Белый' },
+    { id: 6, name: 'Мого', car: 'Самолет', carNumber: 'О634ЕА', carColor: 'Черный' },
+    { id: 7, name: 'Марат', car: 'Самокат', carNumber: 'У980ОТ', carColor: 'Серый' },
+    { id: 8, name: 'Челобай', car: 'Гьоко', carNumber: '123', carColor: 'Белый' },
+    { id: 9, name: 'Паша', car: 'Merkedek', carNumber: '980', carColor: 'Белый' },
+    { id: 10, name: 'Мого', car: 'Самолет', carNumber: 'О634ЕА', carColor: 'Черный' },
+    { id: 11, name: 'Марат', car: 'Самокат', carNumber: 'У980ОТ', carColor: 'Серый' },
+    { id: 12, name: 'Челобай', car: 'Гьоко', carNumber: '123', carColor: 'Белый' },
+  ]
+
+  const driversWithLabels = drivers.map(({ id, name, car, carNumber, carColor }) => {
+    return { id: id, label: name + ', ' + carColor + ' ' + car + ' ' + carNumber }
+  })
+
+  const allOption = { id: 0, label: 'Любое' };
+
+  const driversWithAllOption = [allOption, ...driversWithLabels];
+
+  const driversFinalList = driversWithAllOption;
+
+  const updateFromRoute = (e) => {
+    setFromRoute(e.target.value)
+  }
+
+  const updateSelectedDrivers = (e) => {
+    const options = [...e.target.selectedOptions]
+    const driverIds = options.map(option => option.value)
+    // if (driverIds.length > 1) debugger;
+    // debugger;
+    // if (driverIds.includes(String(allOption.id))) {
+    //   setSelectedDriverIds(driversFinalList.map(driver => String(driver.id)))
+    // } else {
+    setSelectedDriverIds(driverIds)
+    // }
+
+  }
+
+  const selectedDrivers = driversFinalList.filter(driver => selectedDriverIds.includes(String(driver.id)))
+  const selectedDriverLabels = selectedDrivers.map(driver => driver.label)
+
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        <span >Откуда:</span> <input value={fromRoute} onChange={updateFromRoute} /> <span>*</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <span>Авто:</span>
+        <select name="drivers" id="CallPage-drivers" multiple value={selectedDriverIds} onChange={updateSelectedDrivers}>
+          {driversFinalList.map(driver => <option value={driver.id} key={driver.id}>{driver.label}</option>)}
+        </select>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div>
+        <div>From route: <span>{fromRoute}</span></div>
+        <div>Driver ids: <span>{selectedDriverIds}</span></div>
+        <div>Driver: {selectedDriverLabels.map(label => <div>{label}</div>)}</div>
+      </div>
+    </div>
   )
 }
 
