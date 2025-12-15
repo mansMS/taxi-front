fe6edfa1a8ca7a969111656b6d8035acbe22c544
mansms <47422885+mansMS@users.noreply.github.com>
2025-12-15 20:39:27 +0300

    46

--- src/App.jsx
+++ src/App.jsx
@@ -69,8 +69,10 @@ const App = () => {
     <div className='App'>
       <div className='qwe qwe1'></div>
       <div className='qwe qwe2'></div>
-      <div className='qwe qwe3' style={{ height: top_indent }}></div>
-      <div className='qwe qwe4' style={{ height: window.visualViewport?.height }}></div>
+      <div className='qwe qwe3'></div>
+      <div className='qwe qwe4'></div>
+      <div className='qwe qwe5' style={{ height: top_indent }}></div>
+      <div className='qwe qwe6' style={{ height: window.visualViewport?.height }}></div>
       <div>
         <span >Откуда:</span> <input value={fromRoute} onChange={updateFromRoute} /> <span>*</span>
       </div>
